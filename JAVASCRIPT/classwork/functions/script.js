a40992afcf599795677d4d1ddc46c0f49d4bf8c8
akhilchandps <akhilchandps@gmail.com>
2024-10-03 23:47:59 +0530

    commit

---
+++ JAVASCRIPT/classwork/functions/script.js
@@ -0,0 +1,32 @@
+//type conversion or coercion
+
+console.log("20"+5)
+
+console.log("20" -5)
+
+console.log("20"*5)
+
+console.log("20"/5)
+
+console.log(true+1)
+
+console.log(Number("42"));
+
+console.log(Number("Hello"));
+
+console.log(typeof String(123));
+
+console.log(String(true));
+
+console.log(Boolean(0))
+
+console.log(Boolean(''))
+
+console.log(Boolean("hello"))
+
+console.log(Boolean(1))
+
+console.log(parseFloat("15.99"));
+console.log(parseInt("15.99"));
+
+
